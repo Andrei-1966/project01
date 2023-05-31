152765f31c4405224f126618ff09f4c4576555d4
Andrei-1966 <alex_0766@mail.ru>
2023-05-31 12:40:44 +0300

    The task_2.2 was updated according comments recieved , pls check new version

--- F_task_2.2.py
+++ F_task_2.2.py
@@ -6,27 +6,30 @@
 # месяц 6 (июнь) является частью второго квартала; 
 # месяц 11 (ноябрь) является частью четвертого квартала.
 
-def quarter_of(month):
-    pass
 
 user_input = input('Введите номер месяца:')
-month = int(user_input)
 
 q_1 = {1:'январь', 2:'февраль', 3:'март'}
 q_2 = {4:'апрель', 5:'май', 6:'июнь'}
 q_3 = {7:'июль', 8:'август', 9:'сентябрь'} 
 q_4 = {10:'октябрь', 11:'ноябрь', 12:'декабрь'}
- 
 
-for month_k, month_n in q_1.items():
-    if month == month_k:
-        print('Месяц номер', month_k,'(', month_n,') является частью первого квартала', end='') 
-for month_k, month_n in q_2.items():
-    if month == month_k:
-        print('Месяц номер', month_k,'(', month_n,') является частью второго квартала', end='') 
-for month_k, month_n in q_3.items():
-    if month == month_k:
-        print('Месяц номер', month_k,'(', month_n,') является частью третьего квартала', end='') 
-for month_k, month_n in q_4.items():
-    if month == month_k:
-        print('Месяц номер', month_k,'(', month_n,') является частью четвертого квартала', end='')         
+month = int(user_input)
+
+def quarter_of(month):  
+    
+    for month_k, month_n in q_1.items():
+        if month == month_k:
+            print('Месяц номер', month_k,'(', month_n,') является частью первого квартала', end='') 
+    for month_k, month_n in q_2.items():
+        if month == month_k:
+            print('Месяц номер', month_k,'(', month_n,') является частью второго квартала', end='') 
+    for month_k, month_n in q_3.items():
+        if month == month_k:
+            print('Месяц номер', month_k,'(', month_n,') является частью третьего квартала', end='') 
+    for month_k, month_n in q_4.items():
+        if month == month_k:
+            print('Месяц номер', month_k,'(', month_n,') является частью четвертого квартала', end='')  
+    return ''               
+
+print(quarter_of(month))            
