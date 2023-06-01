acd7aa15c68f91c26649aae6a1f4a76e02eb8db4
Andrei-1966 <alex_0766@mail.ru>
2023-06-01 11:05:52 +0300

    The task_1.1 was modified in line with superviser's comments

--- A_task_1.1.py
+++ A_task_1.1.py
@@ -3,10 +3,10 @@
 # Есть строка с перечислением песен
 
 my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
-
+my_favorite_songs = my_favorite_songs.split(',')
+print(my_favorite_songs)
 # Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
 # Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
-my_favorite_songs = ['Waste a Moment', 'Staying\' Alive', 'A Sorta Fairytale', 'Start Me Up', 'New Salvation']
 len(my_favorite_songs)
 print(my_favorite_songs[0] , end=' ')
 print(my_favorite_songs[4] , end=' ')
@@ -15,5 +15,8 @@ print(my_favorite_songs[3])
 
  
 
+# Comment of superviser:
+# К сожалению, переопределять переменную my_favorite_songs нельяз по услвоию задачи!
+# Постарайтесь перейти от строки my_favorite_songs к списку с помощью метода для строк!
 
-
+#  UPDATED
